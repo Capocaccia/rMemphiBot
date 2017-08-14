da9fcbead41c207d4dbd0b08fded309cdf3edb40
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2017-08-14 14:01:17 -0500

    updates header, footer, and response text

--- membot.py
+++ membot.py
@@ -14,8 +14,8 @@ import bs4
 path = '/Users/Carter/code/rMemphiBot/commented.txt'
 # Location of file where id's of already visited comments are maintained
 
-header = 'Header Here'
-footer = 'Footer Here'
+header = 'MemBot provides information about the city of Memphis Tennessee to /r/Memphis users. /n'
+footer = '/n MemBotpytho is provided by /u/CaptainInsane-o.  Source code available upon request.'
 # Text to be posted along with comic description
 
 
@@ -60,7 +60,7 @@ def run_membot(reddit):
             file_obj_r = open(path,'r')
 
             try:
-                explanation = 'BARBECUE INFO'
+                explanation = '<p><strong>BBQ</strong></p><p><a href="https://www.yelp.com/biz/tops-bar-b-q-memphis-12">Tops Bar-B-Q</a> - Ask for white chop, good burgers  </p><p><a href="https://www.yelp.com/biz/central-bbq-memphis-3">Central BBQ</a> – 3 locations, downtown, summer ave and midtown. Nachos with house chips  </p><p><a href="https://www.yelp.com/biz/paynes-bar-b-que-memphis">Payne’s Bar-B-Que</a> – Slaw Dog, BBQ Baloney, CASH ONLY</p><p><a href="https://www.yelp.com/biz/the-bar-b-q-shop-memphis-5">The Bar-B-Q Shop</a> – BBQ Spagetti  </p><p><a href="https://www.yelp.com/biz/corkys-ribs-and-bbq-memphis-2">Corky’s Ribs &amp; BBQ</a> – Ribs and Catfish  </p><p><a href="https://www.yelp.com/biz/cozy-corner-restaurant-memphis-20">Cozy Corner Restaurant</a> – Cornish hen and rib tips  </p><p><a href="http://www.commissarybbq.com">Commissary BBQ</a> – Germantown</p><p><a href="https://www.yelp.com/biz/leonards-pit-barbecue-memphis">Leonard’s Pit Barbecue</a> - buffet  </p><p><a href="https://www.yelp.com/biz/interstate-barbecue-memphis">Interstate Barbecue</a> – BBQ spaghetti  </p><p><a href="https://www.yelp.com/biz/one-and-only-bbq-memphis-4">One &amp;  Only BBQ</a>  </p><p><a href="https://www.yelp.com/biz/toms-barbecue-and-deli-memphis-3">Tom’s Barbecue and Deli</a> – rib tips  Featured on Diners Drive Ins and Dives</p><p><em>TOURIST BBQ</em> <a href="https://www.yelp.com/biz/charlie-vergos-rendezvous-memphis">Rendezvous BBQ</a> – they don’t slow smoke their ribs, they grill them  </p>'
             except:
                 print('Exception!!! Possibly incorrect xkcd URL...\n')
                 # Typical cause for this will be a URL for an xkcd that does not exist (Example: https://www.xkcd.com/772524318/)
@@ -91,3 +91,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
